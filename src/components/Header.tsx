5d32421a2c6107154dbd2ed0a445efb78f2f24d1
jdaschner <daschner02@hotmail.com>
2024-09-11 17:28:39 +0000

    Fixed navbar.

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,28 +1,32 @@
 import React from "react";
 
 const Header: React.FC = () => {
-	return (
-		<header className="bg-base-100 shadow-md">
-			<nav className="navbar px-4">
-				<div className="flex-1">
-					<a className="text-2xl font-bold">Apricot Code</a>
-				</div>
-				<div className="flex-none">
-					<ul className="menu menu-horizontal p-0">
-						<li>
-							<a href="/">Home</a>
-						</li>
-						<li>
-							<a href="/about">About</a>
-						</li>
-						<li>
-							<a href="/contact">Contact</a>
-						</li>
-					</ul>
-				</div>
-			</nav>
-		</header>
-	);
+    return (
+        <header className="bg-base-100 shadow-md">
+            <div className="navbar bg-base-100">
+                <div className="flex-1">
+                    <a className="btn btn-ghost text-xl">Code Apricot</a>
+                </div>
+                <div className="flex-none">
+                    <button className="btn btn-square btn-ghost">
+                        <svg
+                            xmlns="http://www.w3.org/2000/svg"
+                            fill="none"
+                            viewBox="0 0 24 24"
+                            className="inline-block h-5 w-5 stroke-current"
+                        >
+                            <path
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                                strokeWidth="2"
+                                d="M5 12h.01M12 12h.01M19 12h.01M6 12a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0zm7 0a1 1 0 11-2 0 1 1 0 012 0z"
+                            ></path>
+                        </svg>
+                    </button>
+                </div>
+            </div>
+        </header>
+    );
 };
 
 export default Header;
