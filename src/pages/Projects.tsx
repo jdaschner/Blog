ce672916ea82e94b75de7f01c07956f1aa0cd931
jdaschner <daschner02@hotmail.com>
2024-09-11 19:45:01 +0000

    Added pages.

---
+++ src/pages/Projects.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Projects: React.FC = () => {
+	return (
+		<div className="text-center">
+			<h1 className="text-4xl font-bold">Welcome to MyApp</h1>
+			<p className="mt-4">
+				This is a standard layout using Tailwind and DaisyUI.
+			</p>
+		</div>
+	);
+};
+
+export default Projects;
